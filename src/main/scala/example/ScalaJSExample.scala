e1a5328572d11e714418bd0733c0ae13681ba076
Ravi <rkumar@g.hmc.edu>
2015-07-02 15:18:43 +0900

    combat mostly

--- src/main/scala/example/ScalaJSExample.scala
+++ src/main/scala/example/ScalaJSExample.scala
@@ -4,7 +4,7 @@ import org.scalajs.dom
 import org.scalajs.dom.html
 
 import objects._
-
+import scala.math.{max}
 
 
 
@@ -26,6 +26,9 @@ object ScalaJSExample
 		val keysDown = collection.mutable.Set[Int]()
 		val ctx = canvas.getContext("2d").asInstanceOf[dom.CanvasRenderingContext2D]
 
+		ctx.font = "75px sans-serif"
+
+
 		//Make the game
 		val g = new Game(GVs.GAMEX, GVs.GAMEY)
 
@@ -37,8 +40,8 @@ object ScalaJSExample
 		g.genMap()
 
 		//MAKEA THA ZOMBIE
-		//val zed = new Zombie(20, 20, GVs.NORMUNITSIZE, GVs.NORMUNITSIZE, 20, player)
-		//g.addActor(zed)
+		// val zed = new Zombie(20, 20, 20, g.player)
+		// g.addActor(zed)
 
 		val zedSpawner = new ZombieSpawner(20, 20, 20)
 		g.addActor(zedSpawner)
@@ -46,23 +49,38 @@ object ScalaJSExample
 
 		def clear() = 
 		{
-			ctx.fillStyle = "black"
-			ctx.fillRect(0, 0, GVs.GAMEX, GVs.GAMEY)
+			ctx.fillStyle = s"rgb(200, 200, 200)"
+			ctx.fillRect(0, 0, GVs.FULLX, GVs.FULLY)
 		}
 
 		def run()
 		{
-			//handle player movement
-			handlePlayerMovement(g.player, keysDown, g)
+			//Check if the game is over
+			if(g.player.hp <= 0)
+			{
+				//it's over
+				ctx.fillStyle = "white"
+				ctx.fillText("It's over!", GVs.GAMEX/2, GVs.GAMEY/2)
+			}
+			else
+			{
+				//handle player movement
+				handlePlayerMovement(g.player, keysDown, g)
 
-			//Now handle all other ais
-			g.runAllAIs()
+				//Now handle all other ais
+				g.runAllAIs()
 
-			//Clear the screen
-			clear()
+				//Clear the screen
+				clear()
 
-			//Draw the map
-			g.drawAll(ctx)
+				//Draw health bars and such
+				ctx.fillStyle = s"rgb(200, 0, 0)"
+				ctx.fillRect(GVs.GAMEX, 0, max((GVs.FULLX - GVs.GAMEX) * g.player.hp * 1.0/g.player.maxHp, 0), 80)
+				
+
+				//Draw the map
+				g.drawAll(ctx)
+			}
 		}
 
 		clear()
--- src/main/scala/example/objects.scala
+++ src/main/scala/example/objects.scala
@@ -1,17 +1,20 @@
 package objects
 
 import org.scalajs.dom
-import scala.math.{abs, signum}
+import scala.math.{abs, signum, sqrt, pow}
 
 object GVs
 {
 	val GAMEX = 600
 	val GAMEY = 600
 
+	val FULLX = 800
+	val FULLY = 800
+
 	val NORMUNITSIZE = 20
 }
 
-
+case class SimpleLine(start : (Int, Int), end : (Int, Int), color : String)
 
 
 class Obj(locX_ : Int, locY_ : Int, sizeX_ : Int, sizeY_ : Int)
@@ -51,12 +54,17 @@ class Obj(locX_ : Int, locY_ : Int, sizeX_ : Int, sizeY_ : Int)
 
 class Actor(locX_ : Int, locY_ : Int, 
 			sizeX_ : Int, sizeY_ : Int,
-			hp_ : Int, name_ : String)
+			hp_ : Int, faction_ : String,
+			name_ : String)
 extends Obj(locX_, locY_, sizeX_, sizeY_)
 {
 	var maxHp = hp_
 	var hp = hp_
 	var name = name_
+
+	var faction = faction_
+
+	var momentum = (0, 0)
 
 	def changeLoc(newX : Int, newY : Int) =
 	{
@@ -70,7 +78,7 @@ extends Obj(locX_, locY_, sizeX_, sizeY_)
 		{
 			if(o != this && o.blocksMovement && o.collides(newX, newY, sizeX, sizeY))
 			{
-				dom.console.log(name + "can't move there!")
+				dom.console.log(name + " can't move there!")
 				return false
 			}
 		}
@@ -81,13 +89,13 @@ extends Obj(locX_, locY_, sizeX_, sizeY_)
 	{
 		var moved = false
 		//Check collisions
-		if(canMoveTo(locX + dx, locY, g)) //check x movement
+		if(dx != 0 && canMoveTo(locX + dx, locY, g)) //check x movement
 		{
 			locX += dx
 			moved = true
 		}
 
-		if(canMoveTo(locX, locY + dy, g))
+		if(dy != 0 && canMoveTo(locX, locY + dy, g))
 		{
 			locY += dy
 			moved = true
@@ -103,16 +111,97 @@ extends Obj(locX_, locY_, sizeX_, sizeY_)
 	}
 
 	//Has the unit's ai pick a move and then does the move
-	def aiMove(g : Game) = {}
+	def aiMove(g : Game) =
+	{
+		//Knockback
+		val changeX = momentum._1 * 3 / 4
+		val changeY = momentum._2 * 3 / 4
+		moveLoc(changeX, changeY, g)
+		momentum = (changeX, changeY)
+	}
+
+	def takeDamage(source : Actor, damage : Int, g : Game) =
+	{
+		dom.console.log(source.name + " does " + damage + " damage to " + name)
+		hp -= damage
+
+		//Now compute knockback
+		val dx = -1 * signum(source.locX - locX)
+		val dy = -1 * signum(source.locY - locY)
+
+		momentum = (dx * damage, dy * damage)
+
+		//Are we dead?
+		if(hp <= 0) //we're dead
+		{
+			g.acts remove this
+			g.objs remove this
+		}
+	}
+}
+
+class Human(locX_ : Int, locY_ : Int, 
+			hp_ : Int)
+extends Actor(locX_, locY_, GVs.NORMUNITSIZE, GVs.NORMUNITSIZE, hp_, "human", "human")
+{
+	var timeToNextShot = 0
+	var shotCooldown = 20
+	override def draw(ctx : dom.CanvasRenderingContext2D) =
+	{
+		ctx.fillStyle = "red"
+		ctx.fillRect(locX, locY, sizeX, sizeY)
+	}
+
+	override def aiMove(g : Game) =
+	{
+		//Knockback
+		val changeX = momentum._1 * 3 / 4
+		val changeY = momentum._2 * 3 / 4
+		moveLoc(changeX, changeY, g)
+		momentum = (changeX, changeY)
+
+		//Now, check if we can SHOOT STUFF
+		if(timeToNextShot == 0)
+		{
+			var closestAct : Actor = g.acts.head
+			var distance = 100000
+			for(a <- g.acts)
+			{
+				if(a.faction != "NA" && a.faction != faction)
+				{
+					val dist = sqrt(pow(a.locX - locX, 2) + pow(a.locY - locY, 2))
+					if(dist < distance) //new closest
+					{
+						closestAct = a
+						distance = dist.toInt //TODO: CHECK FOR LINE OF SIGHT
+					}
+				}
+			}
+
+			if(distance <= 200)
+			{
+				//SHOOOOT ITTTT
+				closestAct.takeDamage(this, 10, g)
+				timeToNextShot = shotCooldown
+
+				//add it to the graphics
+				g.linesToDraw += SimpleLine((locX, locY), (closestAct.locX, closestAct.locY), "black")
+			}
+		}
+		else
+		{
+			timeToNextShot -= 1
+		}
+	}
 }
 
 
 
+
 class Zombie(locX_ : Int, locY_ : Int, 
-			sizeX_ : Int, sizeY_ : Int,
 			hp_ : Int,
 			target_ : Actor)
-extends Actor(locX_, locY_, sizeX_, sizeY_, hp_, "zombie")
+extends Actor(locX_, locY_, GVs.NORMUNITSIZE, GVs.NORMUNITSIZE, hp_, "zombie", "zombie")
 {
 	var target = target_
 
@@ -124,17 +213,32 @@ extends Actor(locX_, locY_, sizeX_, sizeY_, hp_, "zombie")
 
 	override def aiMove(g : Game) =
 	{
+		//Knockback
+		val changeX = momentum._1 * 3 / 4
+		val changeY = momentum._2 * 3 / 4
+		moveLoc(changeX, changeY, g)
+		momentum = (changeX, changeY)
+
+
 		//Get which way we should move
 		val dx = signum(target.locX - locX)
 		val dy = signum(target.locY - locY)
 
+		//try to move there, see if it works
 		moveLoc(dx, dy, g)
+		
+		if(target.collides(locX + dx, locY + dy, sizeX, sizeY))
+		{
+			//Hell yeah we are, bit him!
+			//g.player.hp -= 10 //ZOMBIEDAMAGE
+			target.takeDamage(this, 10, g)
+		}
 	}
 }
 
 class ZombieSpawner(locX_ : Int, locY_ : Int, 
 			spawnRate_ : Int)
-extends Actor(locX_, locY_, GVs.NORMUNITSIZE, GVs.NORMUNITSIZE, -1, "zombie spawner")
+extends Actor(locX_, locY_, GVs.NORMUNITSIZE, GVs.NORMUNITSIZE, -1, "NA", "zombie spawner")
 {
 	val spawnRate = spawnRate_
 	var timeToNextSpawn = spawnRate
@@ -153,7 +257,7 @@ extends Actor(locX_, locY_, GVs.NORMUNITSIZE, GVs.NORMUNITSIZE, -1, "zombie spaw
 		if(timeToNextSpawn == 0)
 		{
 			//Spawn a zombie if there's room:
-			val newZed = new Zombie(-100, -100, GVs.NORMUNITSIZE, GVs.NORMUNITSIZE, 20, g.player)
+			val newZed = new Zombie(-100, -100, 20, g.player)
 			if(newZed.canMoveTo(locX, locY, g)) //there's room!
 			{
 				g.addActor(newZed)
@@ -189,13 +293,17 @@ class Game(mSizeX : Int, mSizeY : Int)
 	val mapSizeX = mSizeX
 	val mapSizeY = mSizeY
 
+	//graphics-related
+	val linesToDraw = scala.collection.mutable.Buffer[SimpleLine]()
+
 	//all the things
 	val objs = scala.collection.mutable.Set[Obj]()
 	
 	val acts = scala.collection.mutable.Set[Actor]()
 
 	//make the player
-	val player = new Actor(GVs.GAMEX / 2, GVs.GAMEY/2, GVs.NORMUNITSIZE, GVs.NORMUNITSIZE, 100, "Tim Jones")
+	val player = new Human(GVs.GAMEX / 2, GVs.GAMEY/2, 100)
+	player.name = "Tim Jones"
 	addActor(player)
 
 
@@ -212,14 +320,28 @@ class Game(mSizeX : Int, mSizeY : Int)
 
 	def runAllAIs() =
 	{
-		for(a <- acts)
-		{
-			a.aiMove(this)
-		}
+		acts map(_.aiMove(this))
 	}
 
 	def drawAll(ctx : dom.CanvasRenderingContext2D) =
 	{
+		//draw lines
+		while(! linesToDraw.isEmpty)
+		{
+			val line = linesToDraw.remove(0)
+
+			val (sx, sy) = line.start
+			val (ex, ey) = line.end
+
+			//now draw it
+			ctx.beginPath()
+			ctx.moveTo(sx, sy)
+			ctx.fillStyle = line.color
+			ctx.lineTo(ex, ey)
+			ctx.stroke
+		}
+
+		//Draw all the objects
 		objs map(_.draw(ctx))
 	}
 
